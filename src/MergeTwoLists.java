ef496091546529aa0565d3c2157d853b64f1e026
zhxy <zhxy0163@163.com>
2020-10-18 13:44:38 +0800

    合并链表

--- src/MergeTwoLists.java
+++ src/MergeTwoLists.java
@@ -29,27 +29,20 @@ public class MergeTwoLists {
 
     static class Solution {
         public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-            ListNode node = new ListNode(-1);
-            ListNode p = node;
-            do {
-                if(l1.val<=l2.val){
-                    p.next = l1;
-                    l1 = l1.next;
-                }else {
-                    p.next = l2;
-                    if (l2.next==null){
-                        break;
-                    }
-                }
-                p = p.next;
-            }while (l1.next!=null);
-            if(l1.next == null){
-                p.next = l2;
+            if(l1 == null) {
+                return l2;
             }
-            if (l2.next == null){
-                p.next = l1;
+            if(l2 == null) {
+                return l1;
             }
-            return p.next;
+
+            if(l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            } else {
+                l2.next = mergeTwoLists(l1, l2.next);
+                return l2;
+            }
         }
     }
 
